60d3a445c718876d693629517e639a457fc19054
Chris Jan G. Beboso <janjanbeboso0912@gmail.com>
2023-06-27 23:04:45 +0800

    add data

--- src/data.js
+++ src/data.js
@@ -1,9 +1,34 @@
 export const filters = ["Simple", "Luxury", "Rugged"];
 
 export const vansImages = [
-  { img: "src/assets/images/van%201.jpg" },
-  { img: "src/assets/images/van%202.jpg" },
-  { img: "src/assets/images/van%203.jpg" },
-  { img: "src/assets/images/van%204.jpg" },
-  { img: "src/assets/images/van%205.jpg" },
+  {
+    img: "src/assets/images/van%201.jpg",
+    name: "The Cruiser",
+    tag: "luxury",
+    price: 120,
+  },
+  {
+    img: "src/assets/images/van%202.jpg",
+    name: "Dreamfinder",
+    tag: "Simple",
+    price: 65,
+  },
+  {
+    img: "src/assets/images/van%203.jpg",
+    name: "Beach Bum",
+    tag: "Rugged",
+    price: 80,
+  },
+  {
+    img: "src/assets/images/van%204.jpg",
+    name: "Modest Explorer",
+    tag: "Simple",
+    price: 60,
+  },
+  {
+    img: "src/assets/images/van%205.jpg",
+    name: "Green Wonder",
+    tag: "Rugged",
+    price: 70,
+  },
 ];
